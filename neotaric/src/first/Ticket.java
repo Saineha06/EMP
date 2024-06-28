4f88ee4199dfa5c2046de20480b6ebe9d6bc434b
saineha <sainehamarru27@gmail.com>
2024-06-28 12:38:19 +0530

    hey

---
+++ neotaric/src/first/Ticket.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Ticket {
+    public String movieName;
+    public String movieTicket;
+    public String seatNumber;
+    public String rowNumber;
+
+    public Ticket(String movieName, String movieTicket,String seatNumber, String rowNumber){
+        this.movieName=movieName;
+        this.movieTicket=movieTicket;
+        this.seatNumber=movieName;
+        this.rowNumber=rowNumber;
+
+    }
+    public
+}
