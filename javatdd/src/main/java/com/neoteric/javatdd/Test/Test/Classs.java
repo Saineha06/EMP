bd07f1fbb8f367647cced9655b3cd9703661f3d6
saineha <sainehamarru27@gmail.com>
2024-06-28 12:38:19 +0530

    Basic

---
+++ javatdd/src/main/java/com/neoteric/javatdd/Test/Test/Classs.java
@@ -0,0 +1,6 @@
+package com.neoteric.javatdd.Test.Test;
+
+public class Classs {
+
+   public char[] firstName;
+}
---
+++ javatdd/src/test/java/com/neoteric/javatdd/StudentFormTest.java
@@ -0,0 +1,52 @@
+package com.neoteric.javatdd;
+import com.neoteric.javatdd.Test.Test.Classs;
+import org.junit.jupiter.api.Test;
+
+public class StudentFormTest {
+    @Test
+    public  void test() {
+
+        Classs parent1 = new Classs();
+        char firstName = 's';
+        char secondName = 'i';
+        char ThirdName = 'r';
+        char fourthName = 'i';
+
+        Classs parent2 = new Classs();
+        char firstName1 = 'S';
+        char firstName2 = 'r';
+        char firstName3 = 'i';
+    }
+        @Test
+
+        public  void test1(){
+            Classs parentX =new Classs();
+            char firstNameA='s';
+            char secondNameB='i';
+            char ThirdNameC='r';
+            char fourthNameD='i';
+
+            Classs parentY =new Classs();
+            char firstNameM='S';
+            char firstNameN='r';
+            char firstNameO='i';
+
+        }
+        @Test
+    public  void test3 () {
+        Classs parentX = new Classs();
+        char firstNameA = 's';
+        char secondNameB = 'i';
+        char ThirdNameC = 'r';
+        char fourthNameD = 'i';
+
+        Classs parentY = new Classs();
+        char firstNameM = 'S';
+        char firstNameN = 'r';
+        char firstNameO = 'i';
+    }
+
+
+
+
+    }
